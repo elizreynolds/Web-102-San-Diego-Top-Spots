18b9ee9080fb4f7eeb6429ec7e4f2f1ae6f19646
eecarter <elizcarter91@gmail.com>
2020-01-22 19:08:54 -0500

    this is my commit

--- main.js
+++ main.js
@@ -1,4 +1,18 @@
+
+
 $(document).ready(function() {
+   
 // write your code here
 
-});
+
+$.getJSON('data.json', function (data){ 
+    var html = " ";
+    $.each(data, function (index, data){
+        $("table").append("<tr><td>" 
+        + data.name + "</td><td>" 
+        + data.description + "</td><td><a href=http://wwww.google.com/maps?q" 
+        + data.location +  "><button>Open in Google Maps</button></a></td><td>");
+        });
+    });                              
+ });        
+  
